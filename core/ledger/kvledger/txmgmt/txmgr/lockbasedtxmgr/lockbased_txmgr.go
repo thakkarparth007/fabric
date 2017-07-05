8c521b267ebdbefbef46c9c6dfc2a83dec391d8a
Parth Thakkar <pthakker@in.ibm.com>
2017-07-05 14:52:01 +0530

    Renable locking in ledger
    Revert "Disable locking in ledger"
    
    This reverts commit 48cf8f53be76d9066c9fc794cb1ced02b1903aee.

--- core/ledger/kvledger/txmgmt/txmgr/lockbasedtxmgr/lockbased_txmgr.go
+++ core/ledger/kvledger/txmgmt/txmgr/lockbasedtxmgr/lockbased_txmgr.go
@@ -17,8 +17,10 @@ limitations under the License.
 package lockbasedtxmgr
 
 import (
+	"fmt"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/hyperledger/fabric/common/flogging"
 	"github.com/hyperledger/fabric/core/ledger"
@@ -62,7 +64,7 @@ func (txmgr *LockBasedTxMgr) GetLastSavepoint() (*version.Height, error) {
 func (txmgr *LockBasedTxMgr) NewQueryExecutor() (ledger.QueryExecutor, error) {
 	qe := newQueryExecutor(txmgr)
 
-	/*tmpLogFileLock.Lock()
+	tmpLogFileLock.Lock()
 	blockedReaders++
 	fmt.Fprintf(tmpLogFile, "%s Acquiring Rlock. BlockedW: %d. BlockedR: %d. BlockingR: %d\n", time.Now(), blockedWriters, blockedReaders, blockingReaders)
 	tmpLogFileLock.Unlock()
@@ -73,7 +75,7 @@ func (txmgr *LockBasedTxMgr) NewQueryExecutor() (ledger.QueryExecutor, error) {
 	blockedReaders--
 	blockingReaders++
 	fmt.Fprintf(tmpLogFile, "%s Rlock acquired. BlockedW: %d. BlockedR: %d. BlockingR: %d\n", time.Now(), blockedWriters, blockedReaders, blockingReaders)
-	tmpLogFileLock.Unlock()*/
+	tmpLogFileLock.Unlock()
 
 	return qe, nil
 }
@@ -83,7 +85,7 @@ func (txmgr *LockBasedTxMgr) NewTxSimulator() (ledger.TxSimulator, error) {
 	logger.Debugf("constructing new tx simulator")
 	s := newLockBasedTxSimulator(txmgr)
 
-	/*tmpLogFileLock.Lock()
+	tmpLogFileLock.Lock()
 	blockedReaders++
 	fmt.Fprintf(tmpLogFile, "%s Acquiring Rlock. BlockedW: %d. BlockedR: %d. BlockingR: %d\n", time.Now(), blockedWriters, blockedReaders, blockingReaders)
 	tmpLogFileLock.Unlock()
@@ -93,7 +95,7 @@ func (txmgr *LockBasedTxMgr) NewTxSimulator() (ledger.TxSimulator, error) {
 	blockedReaders--
 	blockingReaders++
 	fmt.Fprintf(tmpLogFile, "%s Rlock acquired. BlockedW: %d. BlockedR: %d. BlockingR: %d\n", time.Now(), blockedWriters, blockedReaders, blockingReaders)
-	tmpLogFileLock.Unlock()*/
+	tmpLogFileLock.Unlock()
 
 	return s, nil
 }
@@ -119,7 +121,7 @@ func (txmgr *LockBasedTxMgr) Shutdown() {
 func (txmgr *LockBasedTxMgr) Commit() error {
 	logger.Debugf("Committing updates to state database")
 
-	/*tmpLogFileLock.Lock()
+	tmpLogFileLock.Lock()
 	blockedWriters++
 	fmt.Fprintf(tmpLogFile, "%s Acquiring Lock. BlockedW: %d. BlockedR: %d. BlockingR: %d\n", time.Now(), blockedWriters, blockedReaders, blockingReaders)
 	tmpLogFileLock.Unlock()
@@ -137,7 +139,7 @@ func (txmgr *LockBasedTxMgr) Commit() error {
 		tmpLogFileLock.Unlock()
 	}()
 
-	defer txmgr.commitRWLock.Unlock()*/
+	defer txmgr.commitRWLock.Unlock()
 	logger.Debugf("Write lock acquired for committing updates to state database")
 	if txmgr.batch == nil {
 		panic("validateAndPrepare() method should have been called before calling commit()")
